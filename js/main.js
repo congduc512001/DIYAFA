bf81970a8ec3d8613d8b7035e17d38906bac358c
congduc512001 <congduc512001@gmail.com>
2024-06-26 20:02:36 +0700

    26/6

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+   header();
+   jQuery(window).scroll(function() {
+       header();
+   });
+    $(document).on("click", ".header_menu", function () {
+       $(".menu").addClass("open");
+       $('body').addClass('none_scroll');
+    });
+    $(document).on("click", ".close", function () {
+        $(".menu").removeClass("open");
+        $('body').removeClass('none_scroll');
+     });
+});
+function header() {
+   // Scroll article
+   jQuery(window).scroll(function(event) {
+       var scroll = jQuery(window).scrollTop();
+       if (scroll > 300) {
+           jQuery('.header_center').addClass('white');
+       } else {
+           jQuery('.header_center').removeClass('white');
+       }
+   });
+}
